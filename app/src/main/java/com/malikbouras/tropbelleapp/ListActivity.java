46828d98ebd89bf7a003f453fb5068f789dcdf07
mbouras <malik.bouras@soprasteria.com>
2018-03-26 09:16:45 +0200

    Ajout des vectors drawables

--- app/src/main/java/com/malikbouras/tropbelleapp/ListActivity.java
+++ app/src/main/java/com/malikbouras/tropbelleapp/ListActivity.java
@@ -3,11 +3,12 @@ package com.malikbouras.tropbelleapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -15,11 +16,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.malikbouras.tropbelleapp.adapters.MyRecyclerAdapter;
+import com.malikbouras.tropbelleapp.behavior.BottomNavigationBehavior;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.security.AccessController.getContext;
 
 /**
  *
@@ -50,6 +50,10 @@ public class ListActivity extends AppCompatActivity {
         RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigationView.getLayoutParams();
+        layoutParams.setBehavior(new BottomNavigationBehavior());
     }
 
     private List<Bean> createMockList() {
